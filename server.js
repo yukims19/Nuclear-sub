a43ff0acca88ce13ec55841ed4b8957e5858ab06
Youxi Li <yol107@ucsd.edu>
2018-08-10 12:17:46 -0700

    Add dependencies

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const port = process.env.PORT || 5000;
 const { Client } = require("pg");
 const escape = require("pg-escape");
 const fetch = require("node-fetch");
+const idx = require("idx");
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString =
+  "postgresql://yukili:56112533@localhost:5432/nuclearsub";
+//process.env.DATABASE_URL;
 
 const client = new Client({ connectionString: connectionString });
 client.connect();
@@ -22,15 +25,23 @@ app.post("/store", (req, res) => {
   } else {
     for (let i = 0; i < data.length; i++) {
       //TODO:Check when login works & fix url
-      /*
-    if (!data[i].expanded.messages[0].payload.listUnsubscribe.http) {
-      continue;
-    }*/
-      const subject = data[i].expanded.messages[0].payload.subject;
-      const from = data[i].expanded.messages[0].payload.from;
-      const to = data[i].expanded.messages[0].payload.to;
-      const url =
-        "https://mandrillapp.com/track/click/30254777/mandrillapp.com?p=eyJzIjoibWNYaVl3NmZFTjlUTlRnUWJ4OXg5NElRbEJNIiwidiI6MSwicCI6IntcInVcIjozMDI1NDc3NyxcInZcIjoxLFwidXJsXCI6XCJodHRwOlxcXC9cXFwvbWFuZHJpbGxhcHAuY29tXFxcL3RyYWNrXFxcL3Vuc3ViLnBocD91PTMwMjU0Nzc3JmlkPTRlZjY5MWM5YWFmNTRmNmM5NzBkMjJkZTFhN2I4NDljLmJUeDIycWhEeFBYTHBNU0V3VjRoTXZVSnh6YyUzRCZyPWh0dHBzJTNBJTJGJTJGd3d3LnNtYXJ0cmVjcnVpdGVycy5jb20lMkZ0ZXJtcy1hbmQtY29uZGl0aW9ucyUyRmpvYnMtZm9yLW1lLXVuc3Vic2NyaWJlJTJGJTNGbWRfZW1haWwlM0R5b2wxMDclMjU0MHVjc2QuZWR1XCIsXCJpZFwiOlwiNGVmNjkxYzlhYWY1NGY2Yzk3MGQyMmRlMWE3Yjg0OWNcIixcInVybF9pZHNcIjpbXCI4N2Y5MjNmMTgyODZmYzk3ODAwYTU3MDdlYTIwMmMzYTA0NTAwZmE0XCJdfSJ9";
+
+      if (
+        !idx(
+          data[i],
+          _ => _.expanded.messages[0].payload.listUnsubscribe.http[0]
+        )
+      ) {
+        console.log("no link!");
+        continue;
+      }
+      const subject = idx(data[i], _ => _.expanded.messages[0].payload.subject);
+      const from = idx(data[i], _ => _.expanded.messages[0].payload.from);
+      const to = idx(data[i], _ => _.expanded.messages[0].payload.to);
+      const url = idx(
+        data[i],
+        _ => _.expanded.messages[0].payload.listUnsubscribe.http[0]
+      );
       values.push(
         "('" + subject + "', '" + from + "', '" + to + "', '" + url + "')"
       );
@@ -66,6 +77,37 @@ app.post("/unsubscribe", async (req, res) => {
       "SELECT id, url FROM emails WHERE status IS NULL limit 5;"
     );
     const emailsRes = await client.query(sql);
+    try {
+      const resData = emailsRes.rows;
+      (async function loop() {
+        for (let i = 0; i < resData.length; i++) {
+          emailReturnList.push(resData[i].url);
+          fetch(resData[i].url)
+            .then(response => console.log(response.status))
+            .catch(error => console.log(error));
+          const sqlStatus = escape(
+            "UPDATE emails SET status = 1 WHERE id = '%s'",
+            resData[i].id
+          );
+          client
+            .query(sqlStatus)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+        }
+      })();
+    } catch (emailsRes) {
+      console.error(emailsRes);
+    }
+  }
+  unsubscribe().then(() => res.send({ unsubscribedEmails: emailReturnList }));
+
+  /*
+  let emailReturnList = [];
+  async function unsubscribe() {
+    const sql = escape(
+      "SELECT id, url FROM emails WHERE status IS NULL limit 5;"
+    );
+    const emailsRes = await client.query(sql);
     const resData = emailsRes.rows;
 
     await Promise.all(
@@ -84,7 +126,7 @@ app.post("/unsubscribe", async (req, res) => {
       })
     );
   }
-  unsubscribe().then(() => res.send({ unsubscribedEmails: emailReturnList }));
+  unsubscribe().then(() => res.send({ unsubscribedEmails: emailReturnList }));*/
 });
 
 app.get("/status", async (req, res) => {
